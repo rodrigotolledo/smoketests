29d0bf115b9ed58829d762b6b133610a2dd72606
Rodrigo <rodrigotolledo@gmail.com>
2018-12-19 01:18:41 +1100

    adding test data example

---
+++ testData/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "adminUser": {
+        "username": "placeholder@placeholder.com",
+        "password": "placeholder",
+    },
+    "normalUser": {
+        "username": "placeholder@placeholder.com",
+        "password": "placeholder",
+    }
+};
